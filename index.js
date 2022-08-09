687478264eace0a99b507359078a32cbc538925b
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-09 17:57:44 -0400

    Initial question prompts

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?', 'Provide a description of your project.', 'Provide installation instructions for your project.', 'Provide any relevant usage information for your project.', 'List any project contributors.', 'What are test instructions for your project?'];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions for your project.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide any relevant usage information for your project.'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'List any project contributors.'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are test instrcutions for your project?'
+        }
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
