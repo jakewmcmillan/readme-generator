3cdccad7a1329b89f208f7363aee9635a456eecb
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-10 17:06:02 -0400

    Editing generate readme function

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# readme-generator
+# Storm Count
+
+## Description
+count storm and mana
+## Table of Contents
+
+## Installation
+rituals and payoffs
+## Usage
+go off
+## Credits
+undefined
+## License
+
+## Badges
+
+## Features
+
+## How to Contribute
+
+## Tests
+keep track of storm count
--- index.js
+++ index.js
@@ -1,55 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const generateMD = ({ title, description, installation, usage, constributors, test }) =>
+`# ${title}
+
+## Description
+${description}
+## Table of Contents
+
+## Installation
+${installation}
+## Usage
+${usage}
+## Credits
+${constributors}
+## License
+
+## Badges
+
+## Features
+
+## How to Contribute
+
+## Tests
+${test}`;
+
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of your project?', 'Provide a description of your project.', 'Provide installation instructions for your project.', 'Provide any relevant usage information for your project.', 'List any project contributors.', 'What are test instructions for your project?'];
-
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of your project.'
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Provide installation instructions for your project.'
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Provide any relevant usage information for your project.'
-        },
-        {
-            type: 'input',
-            name: 'contributors',
-            message: 'List any project contributors.'
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'What are test instrcutions for your project?'
-        }
-    ])
-    .then((data) => {
-        const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide any relevant usage information for your project.'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'List any project contributors.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are test instrcutions for your project?'
+    }
+])
+.then((answers) => {
+    const mdPageContent = generateMD(answers);
 
-        fs.writeFile(filename, json.stringify(data, null, '\t'), (err) =>
-            err ? console.log(err) : console.log('ReadMe successfully created!')
-        );
-    });
+    fs.writeFile('README.md', mdPageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md')
+    );
+}) ;
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-// // TODO: Create a function to initialize app
-// function init() {}
+// TODO: Create a function to initialize app
+function init() {}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
